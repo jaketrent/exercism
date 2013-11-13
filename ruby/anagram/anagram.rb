569fa7194083ee5b3a0871263a2dd29c83515d3e
jaketrent <trent.jake@gmail.com>
2013-11-13 16:30:37 -0700

    wip ruby anagram rev 2

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,3 +1,43 @@
+class Comparison
+
+  def initialize(word1, word2)
+    @word1 = word1.downcase
+    @word2 = word2.downcase
+
+    @letters1 = @word1.split('')
+    @letters2 = @word2.split('')
+  end
+
+  def is_anagram?
+    same_length? and match? and has_same_letters?
+  end
+
+  def has_same_letters?
+    @letters1.each do |w|
+      if @letters2.count(w) > 0
+        _remove_letter @letters2, w
+      else
+        @is_anagram = false
+      end
+    end
+  end
+
+  def match?
+    @word1 == @word2
+  end
+
+  def same_length?
+    @word1.size == @word2.size
+  end
+
+  private
+
+  def _remove_letter(letters, letter)
+    letters.slice! letters.index(letter), 1
+  end
+
+end
+
 class Anagram
   def initialize(word)
     @word = word
@@ -8,42 +48,11 @@ class Anagram
     words.each do |w|
       matches.push w if is_anagram? @word, w
     end
-
     matches
   end
 
   def is_anagram?(word1, word2)
-    is_anagram = true
-
-    if _words_same_length?(word1, word2) and !_words_match?(word1, word2)
-      letters1 = word1.downcase.split('')
-      letters2 = word2.downcase.split('')
-      letters1.each do |w|
-        if letters2.count(w) > 0
-          _remove_letter letters2, w
-        else
-          is_anagram = false
-        end
-      end
-    else
-      is_anagram = false
-    end
-
-    is_anagram
-  end
-
-  private
-
-  def _words_match?(word1, word2)
-    word1.downcase == word2.downcase
-  end
-
-  def _remove_letter(letters, letter)
-    letters.slice! letters.index(letter), 1
-  end
-
-  def _words_same_length?(word1, word2)
-    word1.size == word2.size
+    Comparison.new(word1, word2).is_anagram?
   end
 
 end
