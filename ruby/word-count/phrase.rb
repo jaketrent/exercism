dbfd500665124e0fb764133c6d13b8baa2c5d125
jaketrent <trent.jake@gmail.com>
2013-11-12 20:37:20 -0700

    ruby word count rev 2

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,3 +1,36 @@
+class WordBank
+
+  def initialize(phrase)
+    _fill_bank phrase
+  end
+
+  def word_count
+    @bank
+  end
+
+  private
+
+  def _fill_bank(phrase)
+    @bank = {}
+    phrase.split(/[, ]/).each do |word|
+      word = _clean_word word
+      _add_word(word) if not word.empty?
+    end
+  end
+
+  def _add_word(word)
+    if @bank.has_key? word
+      @bank[word] += 1
+    else
+      @bank[word] = 1
+    end
+  end
+
+  def _clean_word(word)
+    word.gsub(/[^0-9a-zA-Z']+/, '').downcase
+  end
+end
+
 class Phrase
 
   def initialize(phrase)
@@ -5,19 +38,7 @@ class Phrase
   end
 
   def word_count
-    words = {}
-    @phrase.split(/[, ]/).each do |word|
-      word = word.gsub(/[^0-9a-zA-Z']+/, '').downcase
-      if not word.empty?
-        if words.has_key? word
-          words[word] += 1
-        else
-          words[word] = 1
-        end
-      end
-    end
-
-    words
+    WordBank.new(@phrase).word_count
   end
 
 end
