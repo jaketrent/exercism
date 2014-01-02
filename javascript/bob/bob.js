1f765c6302ef9292657f9c5e3f489a91e363bf84
jaketrent <trent.jake@gmail.com>
2014-01-01 20:44:24 -0700

    bob rev 2 js - put responses in sep classes

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,65 +1,134 @@
+var Bob, EmptyResponse, Person, Response;
 
-var __hasProp = {}.hasProperty;
-var __extends = function(child, parent) { 
-  for (var key in parent) { 
-    if (__hasProp.call(parent, key)) 
-      child[key] = parent[key]; 
-  } 
+var __hasProp = {}.hasOwnProperty;
+var __extends = function(child, parent) {
+    for (var key in parent) {
+      if (__hasProp.call(parent, key))
+        child[key] = parent[key];
+    }
 
-  function ctor() { 
-    this.constructor = child; 
-  } 
+    function ctor() {
+      this.constructor = child;
+    }
 
-  ctor.prototype = parent.prototype; 
-  child.prototype = new ctor(); 
-  child.__super__ = parent.prototype; 
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor();
+    child.__super__ = parent.prototype;
 
-  return child; 
+    return child;
 };
 
-var __extends = function (child, parent) {
-  for (var key in parent) {
-      if (__hasProp.call(parent, key)) {
-        child[key] = parent[key];
-      }
+Response = (function() {
+  function Response() {}
+
+  Response.isAppropriate = function(msg) {
+    throw new Error('Must implement Response.isAppropriate');
+  };
+
+  Response.give = function() {
+    throw new Error('Must implement Response.give');
+  };
+
+  return Response;
+
+})();
+
+EmptyResponse = (function(_super) {
+  __extends(EmptyResponse, _super);
+
+  function EmptyResponse() {
+    return EmptyResponse.__super__.constructor.apply(this, arguments);
   }
 
-  function ctor() {
-    this.constructor = child;
+  EmptyResponse.isAppropriate = function(msg) {
+    return msg.trim() === '';
+  };
+
+  EmptyResponse.give = function() {
+    return "Fine. Be that way!";
+  };
+
+  return EmptyResponse;
+
+})(Response);
+
+ThreatenedResponse = (function(_super) {
+  __extends(ThreatenedResponse, _super);
+
+  function ThreatenedResponse() {
+    return ThreatenedResponse.__super__.constructor.apply(this, arguments);
   }
 
-  ctor.prototype = parent.prototype;
-  child.prototype = new ctor();
-  child.__super__ = parent.prototype;
+  ThreatenedResponse.isAppropriate = function(msg) {
+    return msg.match(/[a-zA-Z]+/) && msg.toUpperCase() === msg;
+  };
 
-  return child;
-}
+  ThreatenedResponse.give = function() {
+    return "Woah, chill out!";
+  };
 
-var Person = (function () {
+  return ThreatenedResponse;
 
+})(Response);
+
+QuestionedResponse = (function(_super) {
+  __extends(QuestionedResponse, _super);
+
+  function QuestionedResponse() {
+    return QuestionedResponse.__super__.constructor.apply(this, arguments);
+  }
+
+  QuestionedResponse.isAppropriate = function(msg) {
+    return msg[msg.length - 1] === '?';
+  };
+
+  QuestionedResponse.give = function() {
+    return "Sure.";
+  };
+
+  return QuestionedResponse;
+
+})(Response);
+
+DefaultResponse = (function(_super) {
+  __extends(DefaultResponse, _super);
+
+  function DefaultResponse() {
+    return DefaultResponse.__super__.constructor.apply(this, arguments);
+  }
+
+  DefaultResponse.isAppropriate = function(msg) {
+    return true;
+  };
+
+  DefaultResponse.give = function() {
+    return "Whatever.";
+  };
+
+  return DefaultResponse;
+
+})(Response);
+
+Person = (function() {
   function Person() {}
 
   return Person;
 
 })();
 
-var Bob = (function (_super) {
-
+Bob = (function(_super) {
   __extends(Bob, _super);
 
   function Bob() {
     Bob.__super__.constructor.apply(this, arguments);
+    this.responses = [EmptyResponse, ThreatenedResponse, QuestionedResponse, DefaultResponse];
   }
 
-  Bob.prototype.hey = function (msg) {
-    if (msg.trim() === '') {
-      return 'Fine. Be that way!';
-    } else if (msg.match(/[a-zA-Z]+/) && msg.toUpperCase() === msg) {
-      return 'Woah, chill out!';
-    } else if (msg[msg.length - 1] === '?') {
-      return 'Sure.';
-    } else {
-      return 'Whatever.';
+  Bob.prototype.hey = function(msg) {
+    for(var indx in this.responses) {
+      var res = this.responses[indx];
+      if (res.isAppropriate(msg))
+        return res.give();
     }
   };
 
