35eab9312d1c95e58c3c1bc1ee4bfac28a3ba7ad
jaketrent <trent.jake@gmail.com>
2016-10-27 11:34:29 -0600

    fsharp bob

---
+++ fsharp/bob/Bob.fs
@@ -0,0 +1,11 @@
+﻿module Bob
+
+open System
+open System.Linq
+
+let hey (input : string) =
+    match input with
+      | input when input.Trim() = String.Empty -> "Fine. Be that way!"
+      | input when (input.Any(fun x -> System.Char.IsLetter x)) && input.ToUpper() = input -> "Whoa, chill out!"
+      | input when input.EndsWith("?") -> "Sure."
+      | input -> "Whatever."
